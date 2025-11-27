8bb3b3d0bacc080e88d65d4ccc5e7f546bd89702
matthieu <66738778+Matthieudese78@users.noreply.github.com>
2025-11-27 12:26:16 +0100

    new : symbolic : analytic computation of the kinematic constraints jacobian

---
+++ src/double_pendulum/symbolic.py
@@ -0,0 +1,66 @@
+"""Symbolic calulus for constraints and derivatives."""
+
+# %%
+from typing import Callable
+
+import numpy as np
+import sympy as sp
+
+
+# %% Lagrange multipliers :
+def pendulum_cq() -> Callable[np.ndarray, np.ndarray]:
+    """Computation of the constraint jacobian.
+
+    Args:
+    None
+
+    Returns:
+    Callable
+    """
+    # Holonomic Constraints: sqrt(x^2 + y^2) = l
+    x, y, theta, L = sp.symbols("x y theta L")
+    sp.init_printing(use_unicode=True)
+    f = sp.simplify(sp.sqrt(x**2 + y**2))
+    print(f)
+    dfdx = sp.diff(f, x)
+    dfdy = sp.diff(f, y)
+    print(dfdx)
+    print(dfdy)
+
+    # Dependent and independent variables :
+    # Theta is the independent variable
+    # x , y are the dependent variables
+    # Constraints (should be equal to zero):
+    cx = sp.simplify(x - L * sp.cos(theta))
+    cy = sp.simplify(y - L * sp.sin(theta))
+    # Ordinating the variables : x, y, theta
+    # Gradient of the constraint (d Ci/ d Xj)
+    # C_q = [[ dCx/dx , dCx/dy, dCx/dtheta],
+    #        [ dCy/dx , dCy/dy, dCy/dtheta]]
+    dcxdx = sp.diff(cx, x)
+    print(dcxdx)
+    dcxdy = sp.diff(cx, y)
+    print(dcxdy)
+    dcxdtheta = sp.diff(cx, theta)
+    print(dcxdtheta)
+
+    dcydx = sp.diff(cy, x)
+    print(dcydx)
+    dcydy = sp.diff(cy, y)
+    print(dcydy)
+    dcydtheta = sp.diff(cy, theta)
+    print(dcydtheta)
+
+    cq = sp.Matrix([[dcxdx, dcxdy, dcxdtheta], [dcydx, dcydy, dcydtheta]])
+    # alternative, shorter version :
+    c = sp.Matrix([cx, cy])
+    jc = c.jacobian([x, y, theta])
+    return sp.lambdify((x, y, theta), jc, "numpy")
+
+
+# %%
+if __name__ == "__main__":
+    cq2 = pendulum_cq()
+    print(f"cq2 = {cq2}")
+
+# %%
