0247e3e5d677ef8868222eed661310dbf2a30589
matthieu <66738778+Matthieudese78@users.noreply.github.com>
2025-12-03 16:28:40 +0100

    pendulum : before fresh start

--- src/double_pendulum/pendulum.py
+++ src/double_pendulum/pendulum.py
@@ -3,6 +3,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.linalg as la
 
 from double_pendulum.symbolic import pendulum_cq, pendulum_qc
 
@@ -16,32 +17,45 @@ l1 = 1.0
 
 J = m1 * l1**2
 
-theta = 10.0 * np.pi / 180.0
+theta = -10.0 * np.pi / 180.0
 
 x0 = np.cos(theta)
 y0 = np.sin(theta)
 
 # initial angular velocity in rad :
-dthetadt0 = np.pi / 10.0
-vx0 = 1.0
-vy0 = 20.0
+dthetadt0 = -2.0 * np.pi / 10.0
+vx0 = -dthetadt0 * l1 * np.sin(theta)
+vy0 = dthetadt0 * l1 * np.cos(theta)
 
 
-y = np.array([x0, y0, theta, vx0, vy0, dthetadt0])
+# y = np.array([x0, y0, theta, vx0, vy0, dthetadt0])
+
+# Dummy Lambdas :
+Lambda_x = 0.0
+Lambda_y = 0.0
+Lambda_theta = 0.0
+y = np.array([x0, y0, theta])
+
+dydt = np.array([vx0, vy0, dthetadt0])
 # external forces : Fx , Fy, gamma_z :
 
 F = np.array([0.0, -m1 * g, -m1 * g * l1 * np.cos(theta)])
 
-dydt = np.array([y[3], y[4], y[5], F[0], F[1], F[2]])
+# dydt = np.array([y[3], y[4], y[5], F[0], F[1], F[2]])
 
 M = np.array([[m1, 0, 0], [0, m1, 0], [0, 0, J]])
 
 # Constraints jacobian (numpy fction):
 cq = pendulum_cq(l1)[1]
-# %% Generalized forces associated with constraints (numpy fction):
-qc = pendulum_qc(pendulum_cq(l1)[0])[1]
 
-Qc = qc(x0, y0, theta, vx0, vy0, dthetadt0).ravel().ravel()
+print(f"Cq = {pendulum_cq(l1)[0]}")
+# %% Generalized forces associated with constraints (numpy fction):
+qc = pendulum_qc(l1)[1]
+print(f"Qc = {pendulum_qc(l1)[0]}")
+# qc = pendulum_qc(pendulum_cq(l1)[1])[1]
+
+Qc = qc(theta, dthetadt0).ravel()
+# Qc = qc(x0, y0, theta, vx0, vy0, dthetadt0).ravel()
 
 print(f"Qc = {Qc}")
 
@@ -52,29 +66,67 @@ print(M_aug)
 # %%
 print(F)
 print(Qc)
-f_aug = np.concatenate([F, Qc])
-print(f_aug)
+F_aug = np.concatenate([F, Qc])
+print(F_aug)
 
+# %% Initializing d2ydt2 :
+d2ydt2 = np.dot(la.inv(M_aug), F_aug)
+print(d2ydt2)
+print(np.shape(d2ydt2[:3]))
+
+# %% vector of unknowns including the lagrange multipliers
+# Static computation for initial Lambdas :
+
+y = np.concatenate([y, dydt])
+dydt = np.concatenate([dydt, d2ydt2[:3]])
+# %%
+print(np.shape(y))
+print(np.shape(dydt))
 # %% Euler forward integration
-dt = 0.01
-num_steps = 1000
+dt = 0.001
+num_steps = 10000
 save_discr = 4
 isave = 0
 saved_step = np.zeros((int(np.floor(num_steps / save_discr)), y.shape[0]))
+saved_lambdas = np.zeros((int(np.floor(num_steps / save_discr)), 2))
 for step in range(num_steps):
     y += dydt * dt
+    # compute M_aug:
+    M_aug = np.block([[M, cq(y[0], y[1], y[2]).T], [cq(y[0], y[1], y[2]), np.zeros((2, 2))]])
+    # M_aug = np.concatenate([M, cq(y[0], y[1], y[2]).T])
+    # compute F_aug:
+    Qc = qc(y[2], y[5]).ravel()
+    # F_aug = np.concatenate([F, Qc])
+    F = np.array([0.0, -m1 * g, -m1 * g * l1 * np.cos(y[2])])
+    F_aug = np.concatenate([F, Qc])
+    # compute dydt2:
+    d2ydt2 = la.inv(M_aug) @ F_aug
+    # d2ydt2 = np.dot(la.inv(M_aug), F_aug)
+    # update dydt:
+    dydt = np.concatenate([y[:3], d2ydt2[:3]])
+    # dydt = np.array([y[2], y[3], F[0], F[1]])
+
+    # save step:
     print(f"y = {y}")
-    dydt = np.array([y[2], y[3], F[0], F[1]])
     if step % save_discr == 0:
         saved_step[isave] = y
+        saved_lambdas[isave] = d2ydt2[3:]
         isave += 1
-
+# %%
 saved_step = np.array(saved_step)
-plt.scatter(saved_step[:, 0], saved_step[:, 1], s=4)
+plt.scatter(np.arange(len(saved_step)) * dt, saved_step[:, 1], s=4)
 plt.show()
 
 # %%
-plt.scatter(saved_step[:, 0], saved_step[:, 3], s=4)
+plt.scatter(np.arange(len(saved_step)) * dt, saved_step[:, 2], s=4)
+plt.show()
+# %%
+plt.scatter(np.arange(len(saved_step)) * dt, saved_step[:, 3], s=4)
+plt.show()
+
+# %%
+plt.scatter(np.arange(len(saved_step)), saved_lambdas[:, 0], s=4, label="lambda_x")
+plt.scatter(np.arange(len(saved_step)), saved_lambdas[:, 1], s=4, label="lambda_y")
 plt.show()
 
 # %% plot the lagrangian
--- src/double_pendulum/symbolic.py
+++ src/double_pendulum/symbolic.py
@@ -33,6 +33,7 @@ def pendulum_cq(l1: int) -> Callable[np.ndarray, np.ndarray]:
     # Constraints (should be equal to zero):
     cx = sp.simplify(x - l1 * sp.cos(theta))
     cy = sp.simplify(y - l1 * sp.sin(theta))
+    # ctheta = 0
     # Ordinating the variables : x, y, theta
     # Gradient of the constraint (d Ci/ d Xj)
     # C_q = [[ dCx/dx , dCx/dy, dCx/dtheta],
@@ -51,19 +52,43 @@ def pendulum_cq(l1: int) -> Callable[np.ndarray, np.ndarray]:
     dcydtheta = sp.diff(cy, theta)
     print(dcydtheta)
 
+    # dcthetadx = 0
+    # dcthetady = 0
+    # dcthetadtheta = 0
+
     cq = sp.Matrix([[dcxdx, dcxdy, dcxdtheta], [dcydx, dcydy, dcydtheta]])
+
     # alternative, shorter version :
     c = sp.Matrix([cx, cy])
     jc = c.jacobian([x, y, theta])
     print(f"jc = {jc}")
     print(f"jc.T = {jc.T}")
-    # return sp.lambdify((x, y, theta), jc, "numpy")
     return jc, sp.lambdify((x, y, theta), jc, "numpy")
 
 
 # %%
-def pendulum_qc(cq):
-    """Computes Qc for holonomic constraints (Shabana p.404).
+# def pendulum_qc(cq):
+#     """Computes Qc for holonomic constraints (Shabana edition 1989 p. 166, 293 & p.404).
+
+#     Args:
+#     cq: the constraint jacobian matrix.
+
+#     Returns:
+#     qc : the generalized reaction forces.
+#     """
+#     x, y, theta, dxdt, dydt, dthetadt = sp.symbols("x y theta dxdt dydt dthetadt")
+
+#     qdot = sp.Matrix([dxdt, dydt, dthetadt])
+
+#     qc1 = sp.simplify(cq @ qdot)
+#     qc2 = qc1.jacobian([x, y, theta])
+#     qc = sp.simplify(qc2 @ qdot)
+#     print(f"qc = {qc}")
+#     return qc, sp.lambdify((x, y, theta, dxdt, dydt, dthetadt), qc, "numpy")
+
+
+def pendulum_qc(l1):
+    """Computes Qc for holonomic constraints (Shabana edition 1989 p. 166, 293 & p.404).
 
     Args:
     cq: the constraint jacobian matrix.
@@ -71,21 +96,28 @@ def pendulum_qc(cq):
     Returns:
     qc : the generalized reaction forces.
     """
-    x, y, theta, dxdt, dydt, dthetadt = sp.symbols("x y theta dxdt dydt dthetadt")
-
-    qdot = sp.Matrix([dxdt, dydt, dthetadt])
-
-    qc1 = sp.simplify(cq @ qdot)
-    qc2 = qc1.jacobian([x, y, theta])
-    qc = sp.simplify(qc2 @ qdot)
-    return qc, sp.lambdify((x, y, theta, dxdt, dydt, dthetadt), qc, "numpy")
+    theta, dthetadt = sp.symbols("theta dthetadt")
+    qc = sp.Matrix([l1 * dthetadt**2 * sp.cos(theta), -l1 * dthetadt**2 * sp.sin(theta)])
+    return qc, sp.lambdify((theta, dthetadt), qc, "numpy")
 
 
 # %%
 if __name__ == "__main__":
     l1 = 1.0
     cq2 = pendulum_cq(l1)[0]
-    qc = pendulum_qc(cq2)
-    print(qc[0])
+    # %%
+    # qc = pendulum_qc(cq2)
+    qc = pendulum_qc(l1)[0]
+    print(qc)
+    # %%
+    arg = np.zeros(6)
+    print(arg)
+    cq3 = pendulum_cq(l1)[1](arg[0], arg[1], arg[2])
+    print(cq3)
+    print(f"shape cq3 = {np.shape(cq3)}")
+    # qc = pendulum_qc(cq2)[1](arg[0], arg[1], arg[2], arg[3], arg[4], arg[5])
+    qc = pendulum_qc(l1)[1](arg[0], arg[1])
+    print(f"shape qc = {np.shape(qc.ravel())}")
+    print(f"qc = {qc.ravel()}")
 
 # %%
